9a3989bf63c9fcd9d051d46a2722f8170c0895ab
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2025-09-09 19:44:10 +0100

    chore: fix

--- backend/amplify/hosting/resource.ts
+++ backend/amplify/hosting/resource.ts
@@ -1,7 +1,6 @@
 import { NestedStack, NestedStackProps, RemovalPolicy } from "aws-cdk-lib";
 import { Backend } from "../backend";
 import { BlockPublicAccess, Bucket } from "aws-cdk-lib/aws-s3";
-import { PolicyStatement } from "aws-cdk-lib/aws-iam";
 
 export class WebHostingStack extends NestedStack {
   constructor(backend: Backend, id: string, props?: NestedStackProps) {
@@ -12,7 +11,5 @@ export class WebHostingStack extends NestedStack {
       removalPolicy: RemovalPolicy.DESTROY, // Optional: Remove the bucket on stack deletion
       blockPublicAccess: BlockPublicAccess.BLOCK_ACLS_ONLY, // Allows public access but blocks ACLs
     });
-
-    websiteBucket.policy?.document.addStatements(new PolicyStatement({}));
   }
 }
